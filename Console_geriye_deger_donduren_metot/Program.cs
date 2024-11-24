e22d944874c7a3addb42243136a383a193e77d10
fb_20 <fb_20@ELYASA>
2024-11-24 13:21:51 +0300

    Add project files.

---
+++ Console_geriye_deger_donduren_metot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_geriye_deger_donduren_metot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            islem isl = new islem();
+            isl.topla(9, 12);
+            isl.topla(10, 13);
+
+            Console.WriteLine("\n");
+
+            isl.kare(9);
+            Console.Read();
+        }
+    }
+}
